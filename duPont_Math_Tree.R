8638dc192b0643c2c37adba38f0710af50e5d73d
Zoey Lu <71402093+zoeylu666@users.noreply.github.com>
2022-02-07 15:52:40 -0500

    Add files via upload

---
+++ duPont_Math_Tree.R
@@ -0,0 +1,23 @@
+# Load library
+# install.packages("collapsibleTree")
+library(collapsibleTree) 
+library(tidyverse)
+library(ggplot2)
+
+
+view(duPont)
+head(duPont)
+
+duPont <- read.csv(file.choose(""), header=TRUE)
+#"//uno.oet.udel.edu/CRESP/Projects/CurrentProjects/ZL/ZL Data/ZL_C19_Courses/
+#Course Descriptions/BI Dash/duPont/duPont_Eng_Trajectory_data.csv"
+
+p <- collapsibleTree(df=duPont, c("L1_G9", "L2_G10", "L3_G11", "L4_G12"), 
+                     tooltip = TRUE)
+p
+
+
+
+# save the widget
+library(htmlwidgets)
+saveWidget(p, file=paste0( getwd(""), "duPont_English_CPHNRS_CT.html"))
